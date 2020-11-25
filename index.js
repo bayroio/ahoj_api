549a05a443c05f3345844390cb407e3ce05e71bc
eherrador <edgar.herrador@gmail.com>
2020-11-25 22:16:52 +0000

    minimal changes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const limiter = rateLimit({
 })
 app.use(limiter)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
